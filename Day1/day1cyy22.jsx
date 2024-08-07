198e47af686f01c4e36597858578292b3432f01e
DhanyaSenthil08 <150768479+DhanyaSenthil08@users.noreply.github.com>
2024-08-07 19:21:19 +0530

    Add files via upload

---
+++ Day1/day1cyy22.jsx
@@ -0,0 +1,15 @@
+function array(){
+    let arr1=["North","West","East","South"];
+    alert(arr1);
+
+
+}
+function Day1cyy22()
+{
+    return(
+        <div className="App">
+            <button onClick={array}>array</button>
+        </div>
+    )
+}
+export default Day1cyy22;
